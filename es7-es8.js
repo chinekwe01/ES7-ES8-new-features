c6331e3539a66619186ba9d8309c3a30aa8c64b5
clever_chinekwe <chinekwe_clever@outlook.com>
2023-12-28 14:07:36 +0100

    documentation: object.value new feature in es7 | es8 for working with arrays

--- es7-es8.js
+++ es7-es8.js
@@ -1,9 +1,15 @@
 // String.prototype.padStart/padEnd
  const string = '12345';
- console.log(string.padStart(10, '.'));
- console.log(string.padEnd(10, '.'));
+//  console.log(string.padStart(10, '.'));
+//  console.log(string.padEnd(10, '.'));
 
 // Object.values
+const object = {
+    name: 'John',
+    age: 20,
+    favoriteBooks: [ 'Harry Potter 1, Harry Potter 2']
+}
+// console.log(Object.values(object));
 
 // Object.entries
 
